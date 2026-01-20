f801e126f9dab0fb0b3383cd42bcf0c86bef9af7
[Doheechoi] <rndtn47@gmail.com>
2026-01-20 17:53:07 +0900

    플레이어 컨트롤러 시스템 구성
    ##개요
    플레이어 컨트롤러 구성
    
    ##내용
    -좌클릭,우클릭,휠 업다운을 플레이어 컨트롤러에 연결
    -휠클릭시 카드를 선택할 수 있도록 인덱스를 만들어 3장의 카드중 선택할 수 있게 제작
    -input 연결이 안되서 디버깅용으로 Log 찍어서 해결
    
    closs #14

--- Source/AURA/Private/GameBase/AuraPlayerController.cpp
+++ Source/AURA/Private/GameBase/AuraPlayerController.cpp
@@ -4,7 +4,9 @@
 #include "GameBase/AuraPlayerController.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
+#include "Engine/Engine.h"
 #include "InputActionValue.h"
+#include "GameFramework/PlayerController.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
@@ -14,40 +16,66 @@ void AAuraPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	//우클릭 할당 해제
+	bShowMouseCursor = false;
+	
+	// 핵심: "마우스 캡처를 위한 첫 MouseDown을 소비하지 않음"
+	FInputModeGameOnly Mode;
+	Mode.SetConsumeCaptureMouseDown(false); // ✅ RMB/LMB가 캡처로 씹히는 문제 방지
+	SetInputMode(Mode);
+	
 	//로컬플레이어에 IMC 연결하기
 	if (ULocalPlayer* LP = GetLocalPlayer())
 	{
 		if (UEnhancedInputLocalPlayerSubsystem* Subsys = LP->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
 		{
+			UE_LOG(LogTemp, Warning, TEXT("[CardInput] AddMappingContext called"));
+
 			if (IMC_Card)
 			{
-				Subsys->AddMappingContext(IMC_Card,0);
+				UE_LOG(LogTemp, Warning, TEXT("[CardInput] AddMappingContext called"));
+				Subsys->AddMappingContext(IMC_Card,100);
 			}
+			
+			
+
 		}
 		
 	}
+	
+	UE_LOG(LogTemp, Warning, TEXT("플레이어 컨트롤러 연결"));
 }
 
 void AAuraPlayerController::SetupInputComponent()
 {
 	Super::SetupInputComponent();
 	
+	UE_LOG(LogTemp, Warning, TEXT("[CardInput] SetupInputComponent called"));
+	
 	//플레이어 컨트롤러를 EnhancedInputComponent 로 캐스팅시키기
 	UEnhancedInputComponent* EIC=Cast<UEnhancedInputComponent>(InputComponent);
 	if (!EIC)
 	{
+		UE_LOG(LogTemp, Error, TEXT("[CardInput] InputComponent is NOT EnhancedInputComponent. Class=%s"),
+	   *GetNameSafe(InputComponent));
 		return;
 	}
+	
+	UE_LOG(LogTemp, Warning, TEXT("[CardInput] Assets: LMB=%s RMB=%s Select=%s"),
+	*GetNameSafe(IA_CardLMB),
+	*GetNameSafe(IA_CardRMB),
+	*GetNameSafe(IA_CardSelect));
+
 	
 	//LBM,RBM,휠 트리거로 처리
 	if (IA_CardLMB)
 	{
-		EIC->BindAction(IA_CardLMB, ETriggerEvent::Triggered,this,&ThisClass::OnCardLMB);
+		EIC->BindAction(IA_CardLMB, ETriggerEvent::Started,this,&ThisClass::OnCardLMB);
 	}
 	
 	if (IA_CardRMB)
 	{
-		EIC->BindAction(IA_CardRMB, ETriggerEvent::Triggered,this,&ThisClass::OnCardRMB);
+		EIC->BindAction(IA_CardRMB, ETriggerEvent::Started,this,&ThisClass::OnCardRMB);
 	}
 	
 	if (IA_CardSelect)
@@ -60,17 +88,35 @@ void AAuraPlayerController::SetupInputComponent()
 
 void AAuraPlayerController::OnCardLMB()
 {
-	
+	UE_LOG(LogTemp, Warning, TEXT("[CardInput] LMB Pressed | SelectedIndex=%d"), SelectedIndex);
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 1.5f,
+			FColor::Yellow,
+			FString::Printf(TEXT("[CardInput] LMB | Sel=%d"), SelectedIndex));
+	}
 }
 
 void AAuraPlayerController::OnCardRMB()
 {
+	UE_LOG(LogTemp, Warning, TEXT("[CardInput] RMB Pressed | SelectedIndex=%d"), SelectedIndex);
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 1.5f,
+			FColor::Cyan,
+			FString::Printf(TEXT("[CardInput] RMB | Sel=%d"), SelectedIndex));
+	}
 }
 
 void AAuraPlayerController::OnCardSelect(const FInputActionValue& Value)
 {
 	const float AxisValue = Value.Get<float>();
 	const float Sign = FMath::Sign(AxisValue);
+	
+	UE_LOG(LogTemp, Warning, TEXT("[CardInput] Wheel Axis=%.3f Sign=%.0f | Before=%d"),
+	AxisValue, Sign, SelectedIndex);
 
 	if (Sign > 0.f)
 	{
@@ -79,6 +125,14 @@ void AAuraPlayerController::OnCardSelect(const FInputActionValue& Value)
 	else if (Sign < 0.f)
 	{
 		SelectPrev();   // 휠업 (<)
+	}
+	UE_LOG(LogTemp, Warning, TEXT("[CardInput] Wheel | After=%d"), SelectedIndex);
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 1.5f,
+			FColor::Green,
+			FString::Printf(TEXT("[CardInput] Wheel %.3f | Sel=%d"), AxisValue, SelectedIndex));
 	}
 }
 
